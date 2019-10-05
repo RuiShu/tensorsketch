c0d244bc7744c1db50276511fa0fd1819bc29e9e
Rui Shu <00ruishu@gmail.com>
2019-10-04 21:22:35 -0700

    Clean up setup and readme

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# tensorsketch
-A lightweight library for tensorflow 2.0
+# TensorSketch
+
+TensorSketch is a lightweight deep learning library for tensorflow 2.0. Its goal
+is to be a lightweight TensorFlow deep learning library that brings together
+some of the conveniences of both PyTorch and Keras. If you are accustomed to the
+lifecycle of a typical PyTorch-based project and want an easy way to jump into
+using TensorFlow, then this library is for you :-)
--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-"""Setup file for tensorbayes
+"""Setup file for TensorSketch.
 
 For easy installation and uninstallation, do the following.
 
@@ -16,16 +16,11 @@ import os
 setup(
     name="tensorsketch",
     version="0.0.0",
-    author="Rui Shu",
-    author_email="ruishu@stanford.edu",
-    url="http://www.github.com/RuiShu/tensorsketch",
-    # TODO(ruishu): add download_url
-    # download_url="https://github.com/RuiShu/tensorsketch/archive/0.0.0.tar.gz",
     license="MIT",
     description="Lightweight deep learing library for TensorFlow 2.0",
-    install_requires = [
-        "numpy",
-        "tensorflow"
-    ],
-    packages=find_packages()
+    url="http://www.github.com/RuiShu/tensorsketch",
+    packages=find_packages(),
+    install_requires = ["numpy", "tensorflow"],
+    author="Rui Shu",
+    author_email="ruishu@stanford.edu",
 )
